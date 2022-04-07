453b4c0604ce92589a72006ce273ef634683e0f6
frank-w4 <46450395+frank-w4@users.noreply.github.com>
2022-04-07 13:54:28 -0400

    Update diy-distance-shooter.ino

--- diy-distance-shooter.ino
+++ diy-distance-shooter.ino
@@ -29,12 +29,12 @@ void loop() {
   int max_value = 400;
 
   if (reading < max_value) {
-    Serial.print("reading: ");
-    Serial.println(reading);
+//    Serial.print("reading: ");
+//    Serial.println(reading);
     // del = mapIt(reading);
     del = sqrt(10) * sqrt(reading);
-    Serial.print("delay: ");
-    Serial.println(del);
+//    Serial.print("delay: ");
+//    Serial.println(del);
     
     if (reading > 300) {
       
@@ -55,19 +55,19 @@ void loop() {
       del = 100;
       delay(int(del));
       digitalWrite(6, LOW);
-      delay(200);
+      delay(150);
     } else if (reading < 70 && reading >= 40) {
       digitalWrite(6, HIGH);
       del = 100;
       delay(int(del));
       digitalWrite(6, LOW);
-      delay(250);
+      delay(200);
     } else if (reading < 100 && reading >= 70) { 
       digitalWrite(6, HIGH);
       del = 100;
       delay(int(del));
       digitalWrite(6, LOW);
-      delay(300);
+      delay(250);
     } else if (reading < 150 && reading >= 100) { 
       digitalWrite(6, HIGH);
       del = 100;
@@ -85,8 +85,8 @@ void loop() {
       del = 100;
       delay(int(del));
       digitalWrite(6, LOW);
-      delay(700);
-    } else if (reading > 300 & reading < 1200) {
+      delay(800);
+    } else if (reading > 300) {
       digitalWrite(6, LOW);
     }
     
